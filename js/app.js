f203182024a5a2f22e16b22f0f84bfecf6581c3e
dannygallant <danny.gallant@att.net>
2015-07-08 18:08:13 -0500

    Launch new game from button
    +New Game button to run newGame() function. Currently only works if I
    add the onclick event in the HTML.

--- js/app.js
+++ js/app.js
@@ -12,10 +12,16 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
+  	newGame();  // Start new game when page loads
+
 });
 
+// Start a new game when user clicks +New Game button - class .new 
+// $(".new").onclick=newGame(); 	
+// Can't get this to work from here, but if I add to the HTML in the <li> it works
 
 function newGame() {
+	// Clear #guessList
 	var randomNumber = randomizer(1, 100);
 	console.log(randomNumber);
 };
