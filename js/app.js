2b9d171ccef7f04fa15723d36b912ee5fe24b81f
dannygallant <danny.gallant@att.net>
2015-07-10 09:38:20 -0500

    Game logic complete
    Moved new game (+NEWGAME button) logic into app.js. Completed logic to
    get user entry, test that it’s a number between 1 - 100. If it’s a
    valid entry the user screen is updated and a comparison function is
    called to test the entry against the generated random number and
    provide the user with feedback regarding where their guess stands.

--- js/app.js
+++ js/app.js
@@ -1,6 +1,7 @@
-
 $(document).ready(function(){
 	
+	newGame();  // Start new game when page loads. Must come before ".new" click function. Otherwise the call to newGame() won't work.
+
 	/*--- Display information modal box ---*/
   	$(".what").click(function(){
     	$(".overlay").fadeIn(1000);
@@ -12,23 +13,82 @@ $(document).ready(function(){
   		$(".overlay").fadeOut(1000);
   	});
 
-  	newGame();  // Start new game when page loads
+  	// Start new game, without reloading page, when user clicks +NEWGAME.
+  	$(".new").click(function() {
+  		newGame();
+  	});
 
+
+$("#guessButton").on("click", function(e) {
+  	e.preventDefault();
+  	if (correctGuess === true) {
+  		$("#feedback").text("Please click +NEW GAME to start again. You've already won this game.");
+  	} else {
+		var userGuess = $('#userGuess').val();  // set variable to user's entry in userGuess field
+  		console.log("userGuess" + userGuess);
+  		if (+userGuess >= 1 && +userGuess <= 100) {
+			$("#guessList").append("<li>" + userGuess + "</li>");  // add user's guesses to li in guessList
+			numberOfGuesses ++;  // increment count by 1 & update counter on game output
+			$("#count").text(numberOfGuesses);
+			testAnswer(+userGuess, secretNumber);  // Call comparison function to check user's guess against random generated number
+  		} else {
+  			$("#feedback").text("Please enter a number between 1 and 100.");
+  		};
+	};
+	
+  	$("#userGuess").val('');  // Reset userGuess field 
+  	});
 });
 
-// Start a new game when user clicks +New Game button - class .new 
-// $(".new").onclick=newGame(); 	
-// Can't get this to work from here, but if I add to the HTML in the <li> it works
+// =========================
 
-function newGame() {
-	// Clear #guessList
-	var randomNumber = randomizer(1, 100);
-	console.log(randomNumber);
+// Global variables
+var secretNumber;
+var numberOfGuesses;
+var correctGuess = false;
+
+// Runs when page is loaded. Reset game and user Also called by +NEWGAME button, whithou reloading page.
+var newGame = function() {
+	document.getElementById("userGuess").focus();	// Place cursor in user input field
+	secretNumber = randomizer(1, 100);				// Call random number generator
+	console.log("secretNumber:" + secretNumber);
+	numberOfGuesses = 0;							// Reset number of guess & correct guess variables
+	correctGuess = false;
+	$("#feedback").text("Make your Guess!");		// Reset game information presented to user
+	$("#count").text(numberOfGuesses);
+	$("#guessList").children().remove();
 };
 
 function randomizer(min, max) {
   return Math.floor(Math.random() * (max - min + 1)) + min;
 };
+
+// ==========================
+
+
+var testAnswer = function(guessedNum, correctNum) {
+	if (guessedNum === correctNum){
+		$("#feedback").text("Great job! You guessed it!");
+		correctGuess = true;
+	}
+	else {
+		var difference = Math.abs(guessedNum - correctNum);
+		if (difference < 10) {
+			$("#feedback").text("You're very hot!");
+		} else if (difference < 20) {
+			$("#feedback").text("You're hot!");
+		} else if (difference < 30) {
+			$("#feedback").text("You're Warm!");
+		} else if (difference < 50) {
+			$("#feedback").text("You're Cold!");
+		} else {
+			$("#feedback").text("You're Ice cold!");
+		}
+	}
+};
+
+
+// ==========================
 
 
 
